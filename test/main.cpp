5e65377016ce07b82f8e305b396aa8376dddb006
burtonjz <jared.z.burton@gmail.com>
2023-04-29 23:45:02 -0600

    removed example implementation from test/main.cpp; updated test .hpp files to test-%.hpp;

--- test/main.cpp
+++ test/main.cpp
@@ -2,9 +2,5 @@
 #define BOOST_TEST_MODULE "Synthesthesia Tests"
 
 #include <boost/test/unit_test.hpp>
-#include "key-test.hpp"
-#include "oscillator-base-test.hpp"
-
-BOOST_AUTO_TEST_CASE(my_first_test){
-// implementation
-}
+#include "test-key.hpp"
+#include "test-oscillator-base.hpp"
